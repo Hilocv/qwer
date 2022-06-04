cc36289dbc740d4c4b67891fe8b2dbc07c3dde1d
AnCell <80282904+AnCell98@users.noreply.github.com>
2022-06-03 22:25:49 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ import os
 from JDatabase import JsonDatabase
 import start
 
-permitidos = ["AnCell98"]
+permitidos = ["AnCell_98"]
 
 def filtrar_text(update, context):
     text = update.message.text
